387254c0e9e5f7b9830cbff8a05c048f00e2d20f
Sab1e <72060564+Sab1e-GitHub@users.noreply.github.com>
2024-02-25 19:28:24 +0800

    Add files via upload

---
+++ Suffixer.py
@@ -0,0 +1,82 @@
+import os
+
+countTotal = 0
+countSuccess = 0
+countFail = 0
+
+#修改文件后缀
+def editFileSuffix():
+    global countTotal 
+    global countSuccess 
+    global countFail
+    for path, dir_lst, file_lst in paths:
+        for file_name in file_lst:
+            filename=os.path.join(path, file_name)
+            print("找到文件:"+filename)
+            countTotal+=1
+            if filename.endswith(suffix):
+                countSuccess+=1
+                print("正在修改，原文件名:"+filename)
+                newFilename = filename.split(".bc!")[0]
+                print("新文件名:"+newFilename)
+                os.rename(filename, newFilename)
+            else:
+                countFail+=1
+                print("不是"+suffix+"后缀的文件")
+    result()
+
+def deleteSuffixFile():
+    global countTotal 
+    global countSuccess 
+    global countFail
+    for path, dir_lst, file_lst in paths:
+        for file_name in file_lst:
+            filename=os.path.join(path, file_name)
+            print("找到文件:"+filename)
+            countTotal+=1
+            if filename.endswith(suffix):
+                countSuccess+=1
+                print("正在修改，原文件名:"+filename)
+                os.remove(filename)
+            else:
+                countFail+=1
+                print("不是"+suffix+"后缀的文件")
+    result()
+#start
+
+def result():
+    global countTotal 
+    global countSuccess 
+    global countFail
+    print("\n============RESULT============\n")
+    print("共计找到文件："+str(countTotal))
+    print("操作成功文件："+str(countSuccess))
+    print("操作失败文件："+str(countFail))
+    print("\n============RESULT============\n")
+
+print("\n============SUFFIXER============\n")
+print("欢迎使用Suffixer")
+print("\n============SUFFIXER============\n")
+suffix = input("请输入文件后缀:")
+
+inputPath = input("请输入文件所在的文件夹(回车则默认为本程序所在的目录):")
+if(inputPath==""):
+    paths = os.walk(r"./")
+else:
+    inputPath = r"+inputPath+"
+    paths = os.walk(inputPath)
+#os.walk(r"./")
+
+while(1):
+    countTotal = 0
+    countSuccess = 0
+    countFail = 0
+    selection = input("请输入选项(0=去除文件后缀"+suffix+",1=删除"+suffix+"后缀的文件,2=退出程序)")
+    if(selection=="0"):
+        editFileSuffix()
+    elif(selection=="1"):
+        deleteSuffixFile()
+    elif(selection=="2"):
+        exit()
+    else:
+        print("输入不合法！")
